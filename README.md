eae01d8579d5decd2e8609539aea98a5966a1787
kaojoy123456 <404404asd@gmail.com>
2026-05-21 01:37:07 +0800

    Feat:1.Command-line UI 2.animation 3.json saving

--- README.md
+++ README.md
@@ -4,4 +4,52 @@ A website for club
 
 ## Feature
 
-- Saving Test Animation Frames in a json File.
+- Saving Animation Frames in a json File.
+- Command-line UI
+    - ls-member
+    - help
+    - clear
+- Fast Buttons
+
+## About Json
+
+> NOTE:PadEnd is 15 , which means a column(word) will at least use 15 chars.
+
+### anim(anim.json)
+
+```json
+{
+    "animation_name":[
+        "frame",
+        ...
+    ],
+    ...
+}
+```
+
+```js
+
+StartPlay(element,animation_name);
+
+```
+
+### member list(members.json)
+
+```json
+[
+    ["NAME","DUTY","CLASS"],
+    ...
+]
+```
+
+### help(command_information.json)
+
+```json
+{
+    "command_name":[
+        "command_name",
+        "meaning(description)"
+    ],
+    ...
+}
+```
--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function StartPlay(element,animationName){
     .then(res => res.json())
     .then(data => {
         const frames = data[animationName];
-        const interval = 100;
+        const interval = 150;
 
         let i = 0;
         setInterval(() => {
@@ -36,3 +36,61 @@ function StartPlay(element,animationName){
 }
 
 const logoani = document.getElementById("logoani");
+console.log("hello world")
+StartPlay(logoani,"logo")
+
+const shinput = document.getElementById("shinputbox");
+const shoutput = document.getElementById("shoutput");
+
+
+let commmand_informations = {};
+fetch("command_information.json")
+    .then(res => res.json())
+    .then(data =>{
+        console.log("successful got!")
+        commmand_informations = data;
+        console.log(commmand_informations)
+    })
+
+let memberlist = [];
+fetch("members.json")
+    .then(res => res.json())
+    .then(data=>{
+        memberlist = data;
+    })
+
+shinput.addEventListener("keydown",(e)=>{
+    console.log(e.key)
+    if(e.key == "Enter"){
+        const value = shinput.value;
+        let output;
+        switch(value){
+            case "help":
+                output = "NAME".padEnd(15)+"MEANING\n";
+                for(let key in commmand_informations){
+                    output+=commmand_informations[key][0].padEnd(15);
+                    output+=commmand_informations[key][1];
+                    output+="\n";
+                }
+                shoutput.textContent = output;
+                break;
+            case "clear":
+                shoutput.textContent = "";
+                break;
+            case "ls-member":
+                output = "NAME".padEnd(15)+"DUTY".padEnd(15)+"CLASS\n";
+                for(let i=0;i<memberlist.length;i++){
+                    output+=memberlist[i][0].padEnd(15);
+                    output+=memberlist[i][1].padEnd(15);
+                    output+=memberlist[i][2];
+                    output+="\n";
+                }
+                shoutput.textContent = output;
+                break;
+            default:
+                shoutput.textContent = "Unknown Command: "+value;
+                break;
+        }
+        shinput.value = "";
+    }
+})
